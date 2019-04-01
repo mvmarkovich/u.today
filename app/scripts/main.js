c08daadd4bb5f3ffe09ed73cb4c31c19ceb017c9
mvmarkovich <your_github_email_@email.com>
2019-04-01 20:57:11 +0600

    "Ужимаемый баннер" Сайдбар в Price-index Стили для caption

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -292,3 +292,28 @@ function validate(subscribe__form,subscribe__email) {
 }
 
 
+$(function() {
+
+  $('.dropdown > .caption').on('click', function() {
+    $(this).parent().toggleClass('open');
+  });
+
+  $('.dropdown > .list > .item').on('click', function() {
+    $('.dropdown > .list > .item').removeClass('selected');
+    $(this).addClass('selected').parent().parent().removeClass('open').children('.caption').text( $(this).text() );
+  });
+
+  $(document).on('keyup', function(evt) {
+    if ( (evt.keyCode || evt.which) === 27 ) {
+      $('.dropdown').removeClass('open');
+    }
+  });
+
+  $(document).on('click', function(evt) {
+    if ( $(evt.target).closest(".dropdown > .caption").length === 0 ) {
+      $('.dropdown').removeClass('open');
+    }
+  });
+
+});
+
