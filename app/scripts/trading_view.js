f95f1dd759fb0c6546c28c36a916e90e84b74adc
mvmarkovich <your_github_email_@email.com>
2019-08-22 20:41:10 +0600

    - Правки в index-price

--- app/scripts/trading_view.js
+++ app/scripts/trading_view.js
@@ -1,17 +1,17 @@
 new TradingView.widget(
     {
-            "width": "100%",
-            "height": "100%",
-            "symbol": "NASDAQ:AAPL",
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "Light",
-            "style": "1",
-            "locale": "en",
-            "toolbar_bg": "#f1f3f6",
-            "enable_publishing": false,
-            "allow_symbol_change": true,
-            "container_id": "tradingview_334bc"
+        "width": "100%",
+        "height": "100%",
+        "symbol": "NASDAQ:AAPL",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "Light",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_334bc"
     }
 );
 
