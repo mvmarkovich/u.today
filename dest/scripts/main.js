244d7fc95e86f2c28d4f8fde4f17c2f662d6b340
mrk_mikhail <mixail.mark@gmail.com>
2021-01-06 22:23:58 +0600

    Фиксированный сайдбар правки

--- dest/scripts/main.js
+++ dest/scripts/main.js
@@ -163,72 +163,73 @@
     $temp.remove();
   });
 
-})(jQuery);
+  (function(){
+    var a = document.querySelector('.aside-menu-block'), b = null, K = null, Z = 0, P = 16, N = 16;
+    window.addEventListener('scroll', Ascroll, false);
+    document.body.addEventListener('scroll', Ascroll, false);
+    function Ascroll() {
+      var Ra = a.getBoundingClientRect(),
+          R1bottom = document.querySelector('.main-aside-container').getBoundingClientRect().bottom;
+      if (Ra.bottom < R1bottom) {
+        if (b == null) {
+          var Sa = getComputedStyle(a, ''), s = '';
+          b = document.createElement('div');
+          b.className = "stop";
+          b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+          a.insertBefore(b, a.firstChild);
+          var l = a.childNodes.length;
+          for (var i = 1; i < l; i++) {
+            b.appendChild(a.childNodes[1]);
+          }
+          a.style.height = b.getBoundingClientRect().height + 'px';
+          a.style.padding = '0';
+          a.style.border = '0';
+        }
+        var Rb = b.getBoundingClientRect(),
+            Rh = Ra.top + Rb.height,
+            W = document.documentElement.clientHeight,
+            R1 = Math.round(Rh - R1bottom),
+            R2 = Math.round(Rh - W);
+        if (Rb.height > W) {
+          if (Ra.top < K) {  // скролл вниз
+            if (R2 + N > R1) {  // не дойти до низа
+              if (Rb.bottom - W + N <= 0) {  // подцепиться
+                b.className = 'aside-fixed';
+                b.style.top = W - Rb.height - N + 'px';
+                Z = N + Ra.top + Rb.height - W;
+              } else {
+                b.className = 'aside-stop';
+                b.style.top = - Z + 'px';
+              }
+            } else {
+              b.className = 'aside-stop';
+              b.style.top = - R1 +'px';
+              Z = R1;
+            }
+          } else {  // скролл вверх
+            if (Ra.top - P < 0) {  // не дойти до верха
+              if (Rb.top - P >= 0) {  // подцепиться
+                b.className = 'aside-fixed';
+                b.style.top = P + 'px';
+                Z = Ra.top - P;
+              } else {
+                b.className = 'aside-stop';
+                b.style.top = - Z + 'px';
+              }
+            } else {
+              b.className = '';
+              b.style.top = '';
+              Z = 0;
+            }
+          }
+          K = Ra.top;
+        }
 
-(function(){
-  var a = document.querySelector('.aside-menu-block'), b = null, K = null, Z = 0, P = 16, N = 16;
-  window.addEventListener('scroll', Ascroll, false);
-  document.body.addEventListener('scroll', Ascroll, false);
-  function Ascroll() {
-    var Ra = a.getBoundingClientRect(),
-        R1bottom = document.querySelector('.main-news-block').getBoundingClientRect().bottom;
-    if (Ra.bottom < R1bottom) {
-      if (b == null) {
-        var Sa = getComputedStyle(a, ''), s = '';
-        b = document.createElement('div');
-        b.className = "stop";
-        b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-        a.insertBefore(b, a.firstChild);
-        var l = a.childNodes.length;
-        for (var i = 1; i < l; i++) {
-          b.appendChild(a.childNodes[1]);
-        }
-        a.style.height = b.getBoundingClientRect().height + 'px';
-        a.style.padding = '0';
-        a.style.border = '0';
+        window.addEventListener('resize', function() {
+          a.children[0].style.width = getComputedStyle(a, '').width
+        }, false);
       }
-      var Rb = b.getBoundingClientRect(),
-          Rh = Ra.top + Rb.height,
-          W = document.documentElement.clientHeight,
-          R1 = Math.round(Rh - R1bottom),
-          R2 = Math.round(Rh - W);
-      if (Rb.height > W) {
-        if (Ra.top < K) {  // скролл вниз
-          if (R2 + N > R1) {  // не дойти до низа
-            if (Rb.bottom - W + N <= 0) {  // подцепиться
-              b.className = 'aside-fixed';
-              b.style.top = W - Rb.height - N + 'px';
-              Z = N + Ra.top + Rb.height - W;
-            } else {
-              b.className = 'aside-stop';
-              b.style.top = - Z + 'px';
-            }
-          } else {
-            b.className = 'aside-stop';
-            b.style.top = - R1 +'px';
-            Z = R1;
-          }
-        } else {  // скролл вверх
-          if (Ra.top - P < 0) {  // не дойти до верха
-            if (Rb.top - P >= 0) {  // подцепиться
-              b.className = 'aside-fixed';
-              b.style.top = P + 'px';
-              Z = Ra.top - P;
-            } else {
-              b.className = 'aside-stop';
-              b.style.top = - Z + 'px';
-            }
-          } else {
-            b.className = '';
-            b.style.top = '';
-            Z = 0;
-          }
-        }
-        K = Ra.top;
-      }
-      window.addEventListener('resize', function() {
-        a.children[0].style.width = getComputedStyle(a, '').width
-      }, false);
     }
-  }
-})()
+  })()
+
+})(jQuery);
